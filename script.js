e4be39437de152f4dfc76ba9ecc005d0413a1fdd
Xmeme <phamtracanh36@gmail.com>
2024-05-30 21:57:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
-const firstNames = ["Nguyễn", "Trần", "Lê", "Phạm", "Hoàng", "Phan"];
-const lastNames = ["Văn A", "Thị B", "Văn C", "Thị D"];
+const firstNames = ["Emily", "Olivia", "Ava", "Isabella", "Sophia", "Mia"];
+const lastNames = ["Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia"];
 
 function generateName() {
   const randomFirstName = firstNames[Math.floor(Math.random() * firstNames.length)];
   const randomLastName = lastNames[Math.floor(Math.random() * lastNames.length)];
   return {
-    firstname: randomFirstName,
-    lastname: randomLastName
+    firstName: randomFirstName,
+    lastName: randomLastName
   };
 }
 
-// Xử lý yêu cầu GET và POST
-const requestMethod = (typeof event !== 'undefined' && event.request) ? event.request.method : 'GET';
-if (requestMethod === 'GET' || requestMethod === 'POST') {
-  const result = generateName();
-  const jsonResult = JSON.stringify(result);
-  if (typeof event !== 'undefined' && event) {
-    event.respondWith(new Response(jsonResult, {
-      headers: { 'Content-Type': 'application/json' }
-    }));
-  } else {
-    document.write(jsonResult);
-  }
-}
+const result = generateName();
+const jsonResult = JSON.stringify(result);
+document.write(jsonResult);
