c53a94742c7a4f0314fdecb04a34345764b5a223
Xmeme <phamtracanh36@gmail.com>
2024-05-31 18:46:57 +0700

    Update and rename index.js to script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+// Danh sách username mẫu
+const usernames = [
+  "user1",
+  "user2",
+  "user3",
+  "user4",
+  "user5",
+  // Thêm các username khác vào đây
+];
+
+// Hàm lấy một username ngẫu nhiên từ danh sách
+function getRandomUsername() {
+  const randomIndex = Math.floor(Math.random() * usernames.length);
+  return usernames[randomIndex];
+}
+
+// Ghi đè phương thức fetch() để trả về dữ liệu JSON khi truy cập trang
+window.fetch = () => {
+  const randomUsername = getRandomUsername();
+  const jsonData = JSON.stringify({ username: randomUsername });
+  return new Response(jsonData, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
